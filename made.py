5a91063c5e0974af0a9791e8393e2f093e48cce5
hanksalexander1-cyber <hanksalexander1@gmail.com>
2026-02-02 23:28:01 -0500

    Add files via upload

--- made.py
+++ made.py
@@ -1,4 +1,5 @@
 from dice import dice_roller
+import time
 starter = dice_roller(3)
 
 class playerclass:
@@ -9,20 +10,25 @@ class playerclass:
         
 
     def determine_points(self):
+        points_given = 0
         print("your die are as shown")
         values = starter.roll()
+        starter.print_die()
+        print(values)
         for value in values:
-            points_given = 0
-            if values[0] == values[1] and values[1] == values[2]:
-                points_given += 500
-                print("you rolled 3 of a kind and was given 500 points")
-                continue
-            elif value == 1:
+            if value == 1:
                 points_given += 100
-            elif value == 5:
+            if value == 5:
                 points_given += 50
-            else:
-                print("you rolled a farkle and were given 0 points")
+        if values[0] == values[1] and values[1] == values[2]:
+            points_given = 0
+            points_given += 500
+            print("you rolled 3 of a kind and was given 500 points")
+        elif values[0] not in [1,5] and values[1] not in [1,5] and values[2] not in [1,5]:
+            points_given = 0
+            print("you rolled a farkle and were given 0 points")
+            time.sleep(.5)
+            return False
                         
         self.score += points_given
-        return points_given
+        print(f"your score is currently {self.score} points")
